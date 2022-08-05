ce088a8fe89c81e336dd76bd294e87ff7bd3e66c
brucethemoose <AlphaAtlas1@outlook.com>
2022-08-05 01:02:47 -0500

    First working script

--- Benchmark.py
+++ Benchmark.py
@@ -1,42 +1,52 @@
-import os,pexpect,time,itertools
+import os,time,shutil
+import pexpect
+from pexpect import popen_spawn
 
 benchlist = [
-  "command",
-  "command2"
+  r'''"C:/Games/PolyMC-Windows-Portable-1.4.0/graalvm-ee-java17-22.2.0/bin/java.exe" @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.1.68/win_args.txt %* --nogu'''
 ]
 
 pathlist = [
-  "path1",
-  "path2"
+  r"C:/Games/minecraft-server"
 ]
 
-chunkgen_command = r"/forge pregen 5000"
+chunkgen_command = r"/forge generate 0 0 0 500"
 chunkgen_expect =  r"Finished generating"
+
+debug = True
 
 def benchmark(benchcmd, benchpath):
   os.chdir(benchpath)
-  os.rename("world","_worldbackup") #Backup minecraft world
+  if os.path.isdir("world"):
+    if os.path.isdir("_worldbackup"):
+      shutil.rmtree("_worldbackup")
+    os.rename("world","_worldbackup") #Backup minecraft world
   start = time.time()
-  with pexpect.spawn(benchcmd) as child:   #Start Minecraft server
-    child.expect (r'[minecraft/DedicatedServer]: Done (') #Wait until chunk generation is finished
-    startuptime = time.time() - start
-    time.sleep(25)    #Let the server "settle"
-    start = time.time()
-    child.sendline (chunkgen_command)   #Generate chunks
-    child.expect (chunkgen_expect)
-    chunkgentime = time.time() - start
-    child.sendline ('stop')     #shutdown the minecraft server
-    time.sleep(8) # just in case
-  os.remove("world")
-  os.rename("_worldbackup", "world")  #restore backup
+  child = pexpect.popen_spawn.PopenSpawn(benchcmd, timeout=1200, maxread=2000000,)   #Start Minecraft server
+  if debug: print("Starting server...")
+  child.expect_exact(r' Done ') #Wait until chunk generation is finished
+  if debug: print("Server started")
+  startuptime = time.time() - start
+  time.sleep(20)    #Let the server "settle"
+  if debug: print("Generating chunks...")
+  start = time.time()
+  child.sendline(chunkgen_command)   #Generate chunks
+  child.expect_exact(chunkgen_expect)
+  if debug: print("Chunks finished. Stopping server...")
+  chunkgentime = time.time() - start
+  child.sendline('stop')     #shutdown the minecraft server
+  child.wait()
+  shutil.rmtree("world")
+  if os.path.isdir("_worldbackup"):
+    os.rename("_worldbackup", "world")  #restore backup
   return chunkgentime, startuptime
 
 for (bench,path) in zip(benchlist,pathlist):
   ctime,stime = benchmark(bench,path)
-  print("Path: " + path)
-  print("Command: " + bench)
+  print("Path: " + str(path))
+  print("Command: " + str(bench))
   print(" ")
-  print ("Startup Time: " + stime)
-  print("Chunk Generation Time:" + ctime)
+  print ("Startup Time: " + str(stime))
+  print("Chunk Generation Time:" + str(ctime))
   print (" ")
   
