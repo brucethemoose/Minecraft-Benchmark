cbdd3cefba812f613ca9c5e1530ec1a2d6e9098e
brucethemoose <AlphaAtlas1@outlook.com>
2022-08-04 23:16:43 -0500

    Initial benchmark script

--- Benchmark.py
+++ Benchmark.py
@@ -1,4 +1,4 @@
-import os,sys,subprocess,pexpect
+import os,pexpect,time,itertools
 
 benchlist = [
   "command",
@@ -10,16 +10,33 @@ pathlist = [
   "path2"
 ]
 
+chunkgen_command = r"/forge pregen 5000"
+chunkgen_expect =  r"Finished generating"
+
 def benchmark(benchcmd, benchpath):
   os.chdir(benchpath)
-  os.rename("world","_worldbackup")
-  child = pexpect.spawn(benchcmd)
-  child.expect ('Finished Generating World')
-  child.sendline ('anonymous')
-child.expect ('Password:')
-child.sendline ('noah@example.com')
-child.expect ('ftp> ')
-child.sendline ('ls /pub/OpenBSD/')
-child.expect ('ftp> ')
-print child.before   # Print the result of the ls command.
-child.interact()     # Give control of the child to the user.
+  os.rename("world","_worldbackup") #Backup minecraft world
+  start = time.time()
+  with pexpect.spawn(benchcmd) as child:   #Start Minecraft server
+    child.expect (r'[minecraft/DedicatedServer]: Done (') #Wait until chunk generation is finished
+    startuptime = time.time() - start
+    time.sleep(25)    #Let the server "settle"
+    start = time.time()
+    child.sendline (chunkgen_command)   #Generate chunks
+    child.expect (chunkgen_expect)
+    chunkgentime = time.time() - start
+    child.sendline ('stop')     #shutdown the minecraft server
+    time.sleep(8) # just in case
+  os.remove("world")
+  os.rename("_worldbackup", "world")  #restore backup
+  return chunkgentime, startuptime
+
+for (bench,path) in zip(benchlist,pathlist):
+  ctime,stime = benchmark(bench,path)
+  print("Path: " + path)
+  print("Command: " + bench)
+  print(" ")
+  print ("Startup Time: " + stime)
+  print("Chunk Generation Time:" + ctime)
+  print (" ")
+  
